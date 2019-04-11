0f64062e202ec7aed218ff3bbc86f88b3624268b
zhenyus <sunnyszy@gmail.com>
2019-04-11 11:17:26 -0400

    random read from smaller region

--- trafficserver-8.0.3/iocore/cache/CacheRead.cc
+++ trafficserver-8.0.3/iocore/cache/CacheRead.cc
@@ -151,10 +151,8 @@ Cache::open_read(Continuation *cont, const CacheKey *key, CacheHTTPHdr *request,
 
       if (!result.w[0] && !result.w[1] && !result.w[2] && !result.w[3] && !result.w[4]) {
 //        empty.
-        result.w[4] = 0;
-
         //random value from 127 GB space, 4K aligned, don't read around as max is 16MB
-        dir_set_offset(&result, (key->b[0] & 0x1fbffff000ull));
+        dir_set_offset(&result, (key->b[0] & 0xffff000ull));
         //the normal offset
 //        dir_set_offset(&result, 1);
         dir_set_approx_size(&result, vdir_value_len+VDOC_HEADER_LEN);
@@ -166,7 +164,6 @@ Cache::open_read(Continuation *cont, const CacheKey *key, CacheHTTPHdr *request,
         dir_set_head(&result, 1);
         dir_set_pinned(&result, 0);
         dir_set_token(&result, 0);
-        dir_set_next(&result, 0);
         dir_set_next(&result, 0);
         c->od        = nullptr;
       } else {
