c5403b1da53d62ff2fa1f9137398cff5b23b50e6
zhenyus <sunnyszy@gmail.com>
2019-04-18 00:02:29 -0400

    update set server past to forever

--- scripts/set_server_past.sh
+++ scripts/set_server_past.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo timedatectl set-ntp false
-sudo timedatectl set-time 2019-04-18
-echo $(timedatectl status)
+while true; do
+  sudo timedatectl set-ntp false
+  sudo timedatectl set-time 2019-04-18
+  echo $(timedatectl status)
+  sleep 60
+done
 
